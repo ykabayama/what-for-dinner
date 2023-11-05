592b507d56e5c16e2a3e9f01dd487b5e0e536a32
y-kabayama <y-kabayama@b-tm.co.jp>
2023-11-05 15:53:17 +0000

    モック作成1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,117 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-sm-6">
+                <h2 class="text-center">今日の料理</h2>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">唐揚げ</h5>
+                        <p class="card-text">作り方のテスト
+                            テスト
+                            てすと
+                        </p>
+                        <a href="#" class="card-link">詳細</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center mt-5">
+            <div class="col text-center">
+                <h2>今の献立</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">カレーライス</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">料理予定日: 5月4日(水)</h6>
+                        <ul class="card-text">
+                            <li>1つめの材料</li>
+                            <li>2つめの材料</li>
+                            <li>3つめの材料</li>
+                        </ul>
+                        <a href="#" class="card-link">詳細</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">カレーライス</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">料理予定日: 5月4日(水)</h6>
+                        <ul class="card-text">
+                            <li>1つめの材料</li>
+                            <li>2つめの材料</li>
+                            <li>3つめの材料</li>
+                        </ul>
+                        <a href="#" class="card-link">詳細</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">カレーライス</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">料理予定日: 5月4日(水)</h6>
+                        <ul class="card-text">
+                            <li>1つめの材料</li>
+                            <li>2つめの材料</li>
+                            <li>3つめの材料</li>
+                        </ul>
+                        <a href="#" class="card-link">詳細</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center mt-5">
+            <div class="col text-center">
+                <h2>最近作っていないレシピ</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">カレーライス</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">料理予定日: 5月4日(水)</h6>
+                        <ul class="card-text">
+                            <li>1つめの材料</li>
+                            <li>2つめの材料</li>
+                            <li>3つめの材料</li>
+                        </ul>
+                        <a href="#" class="card-link">詳細</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">カレーライス</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">料理予定日: 5月4日(水)</h6>
+                        <ul class="card-text">
+                            <li>1つめの材料</li>
+                            <li>2つめの材料</li>
+                            <li>3つめの材料</li>
+                        </ul>
+                        <a href="#" class="card-link">詳細</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">カレーライス</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">料理予定日: 5月4日(水)</h6>
+                        <ul class="card-text">
+                            <li>1つめの材料</li>
+                            <li>2つめの材料</li>
+                            <li>3つめの材料</li>
+                        </ul>
+                        <a href="#" class="card-link">詳細</a>
+                    </div>
                 </div>
             </div>
         </div>
