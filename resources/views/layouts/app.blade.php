41c27fec8c3d243730b12c1f4eba5cd8e1464142
y-kabayama <y-kabayama@b-tm.co.jp>
2023-11-29 15:37:12 +0000

    fix:bootstrap削除

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/sass/app.scss'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased">
