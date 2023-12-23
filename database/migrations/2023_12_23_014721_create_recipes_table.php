f755a8da8f932c5eecc9b20873dd74a1dc01ba23
yusuke-kabayama <yusukekabayama798@gmail.com>
2023-12-23 13:21:13 +0000

    fix:マイグレーションファイルの修正

--- database/migrations/2023_12_23_014721_create_recipes_table.php
+++ database/migrations/2023_12_23_014721_create_recipes_table.php
@@ -29,7 +29,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('recipes', function (Blueprint $table) {
-            $table->dropForeign('posts_user_id_foreign');
+            $table->dropForeign(['user_id']);
         });
         Schema::dropIfExists('recipes');
     }
